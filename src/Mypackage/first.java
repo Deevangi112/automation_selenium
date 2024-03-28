dbdd2e87cab52c7b713a7be7d174f82ae9979ff7
Deevangi112 <deevangi@snapmint.com>
2024-03-28 21:08:52 +0530

    again add new changes

--- src/Mypackage/first.java
+++ src/Mypackage/first.java
@@ -8,65 +8,48 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class first {
-
-
-	public static void main(String[] args) throws InterruptedException {
+         public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		
 		System.setProperty("webdriver.chrome.driver", "C:\\Automation\\Deiver\\chromedriver.exe");
-        
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
 		driver.get("https://snapmint:ciHns$@NKSa@qa2.snapmint.com/merchant-demo");
-		
-		
-		//driver.get("https://qa.snapmint.com/");
-		
-		//driver.findElement(By.xpath("//contains[@class='flex gap-2 sm:gap-1 items-center']")).click();
-		
-		//maximize browser
+		     System.out.println("open merchant demo page and enter username and password");
 		driver.manage().window().maximize();
-		
-		driver.findElement(By.id("new-mobile")).sendKeys("9191095952");
-		
-		
-		driver.findElement(By.name("merchant_id")).clear();
+		System.out.println("Maximize browser");
+		driver.findElement(By.id("new-mobile")).sendKeys("9178590291");
+		   Thread.sleep(2000);
+		   System.out.println("Enter user number");
+	    driver.findElement(By.name("merchant_id")).clear();
 		driver.findElement(By.name("merchant_id")).sendKeys("4");
-		
+		    Thread.sleep(2000);
+		    System.out.println("Enter merchant id");
 		driver.findElement(By.name("order_id")).clear();
 		driver.findElement(By.name("order_id")).sendKeys("ttti93");
-		
+		  Thread.sleep(2000);
+		  System.out.println("Enter order id");
 		driver.findElement(By.name("order_value")).clear();		
 		driver.findElement(By.name("order_value")).sendKeys("15000");
-		
-		Thread.sleep(1000);
-		
+		  Thread.sleep(2000);
+		  System.out.println("Enter order value");
 		driver.findElement(By.name("full_name")).clear();		
 		driver.findElement(By.name("full_name")). sendKeys("Deevangi test");
-		
-		
+		   Thread.sleep(2000);
+		   System.out.println("Enter user full name");
 		driver.findElement(By.name("email")).clear();	
-		driver.findElement(By.name("email")). sendKeys("q7976k@gmail.com");
-		
-		Thread.sleep(1000);
-			
-		//driver.findElement(By.id("checksum_btn")).click();
-		
+		driver.findElement(By.name("email")). sendKeys("te6976k@gmail.com");
+		   Thread.sleep(1000);
+		   System.out.println("Enter email id");
 		driver.findElement(By.xpath("//input[@id='checksum_btn']")).click();
-		
-		//driver.findElement(By.xpath("//input[@type='submit']")).click();
-		
-			driver.findElement(By.xpath("//input[@value='submit']")).click();
-		//locate password using multiple attribute
-
+		  System.out.println("Click create checksum button");
+		driver.findElement(By.xpath("//input[@value='submit']")).click();
+		  System.out.println("Click submit button");
 		driver.findElement(By.xpath("//input[@id='inputBox'][@name='otp']")).sendKeys("1010");
-		
-		//locate verify button
-		
+		   Thread.sleep(1000);
+           System.out.println("Enter OTP");
 		driver.findElement(By.className("snap-primary-button")).click();
-		
-		//clear first 
+		   System.out.println("Enter Verify button");
+           /*//clear first 
         driver.findElement(By.xpath("//input[@id='firstName']")).clear();
         //enter  first name
         driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("test");
@@ -74,16 +57,32 @@ public class first {
         driver.findElement(By.xpath("//input[@id='lastName']")).clear();
          //enter last name
         driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("user");
-         //enter pan number
-
-		driver.findElement(By.xpath("//input[@id='panNumber']")).sendKeys("FHOPL5898N");
+         */
+        driver.findElement(By.xpath("//input[@id='panNumber']")).sendKeys("FHOPL5898N");
+		   Thread.sleep(1000);
+           System.out.println("Enter PAN number ");
+        driver.findElement(By.xpath("//input[@value='f']")).click(); 
+           Thread.sleep(1000);
+           System.out.println("Select gender");
+        driver.findElement(By.xpath("//div[text()='Day']")).click();
+        Thread.sleep(2000);
+        System.out.println("User click on Day tab");
+              driver.findElement(By.xpath("//*[@id='react-select-2--option-20']")).click();
+        Thread.sleep(2000);
+        System.out.println("User Select Day from Dropdown");
+        WebElement month=driver.findElement(By.xpath("(//*[@class='Select-placeholder'])[1]"));
+              month.click();
+        System.out.println("User click on Month tab");
+              driver.findElement(By.xpath("//*[@id='react-select-3--option-8']")).click();
+        Thread.sleep(2000);
+        System.out.println("User Select Month from Dropdown");
+        WebElement year = driver.findElement(By.xpath("(//*[@class='Select-placeholder'])[1]"));
+               year.click();
+        System.out.println("User click on Year tab");
+               driver.findElement(By.xpath("//*[@id='react-select-4--option-31']")).click();
+        Thread.sleep(2000);
+        System.out.println("User Select Year from Dropdown");
 		
-		//gender
-        WebElement radio=driver.findElement(By.xpath("//input[@value='f']"));
-		
-		radio.click();
-		
-		//driver.quit();	
-	}
+         }	
 
 }
